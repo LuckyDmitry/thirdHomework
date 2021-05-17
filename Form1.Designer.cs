6c457869aeffb68830ee6c21a68dcf591a085abb
unknown <dim.trif1234@gmail.com>
2021-05-17 23:42:13 +0300

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ThirdHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Entry amount";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.InsertButtonClicked);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Select";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SelectButtonClicked);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(265, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 137);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Indexes";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 104);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(53, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "GIN";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 76);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Hash";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "B-tree";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButtonCheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "No";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButtonCheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(174, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 42);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Select by condition";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.SelectByConditionButtonClicked);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 183);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(450, 180);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 49);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Select by 3 col";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1006, 489);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ThirdHomework
+{
+    public partial class Form1 : Form
+    {
+        private string text;
+        private Dictionary<int, String> sentences;
+        private string[] insertQeuries;
+        private Index index;
+        private Npgsql.NpgsqlConnection connection;
+        private Operation operation;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        ~Form1()
+        {
+            connection.Close();
+        }
+
+        private void Init()
+        {
+            string connectionParameters = "Server=localhost;Port=5432;Username=postgres;Password=123;Database=third;";
+            connection = new Npgsql.NpgsqlConnection(connectionParameters);
+            sentences = new Dictionary<int, string>();
+            insertQeuries = new string[0];
+            connection.Open();
+            PopulateStringValues();
+            index = Index.noIndex;
+        }
+
+        public void MakeQuery(string query)
+        {
+            NpgsqlCommand t_CommandIfExist = new NpgsqlCommand(query);
+            t_CommandIfExist.Connection = connection;
+            NpgsqlDataReader reader = t_CommandIfExist.ExecuteReader();
+            reader.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            text = textBox.Text;
+        }
+
+        private void InsertButtonClicked(object sender, EventArgs e)
+        {
+            operation = Operation.insert;
+            int amount = GetValueFromTextField();
+            if (amount > 0) {
+                GenerateInsertQueries(amount);
+                TimeSpan time = TimeMeasure(GenerateEntries, amount);
+                ShowMessage($"Generation finished with {time.TotalSeconds}");
+                listBox1.Items.Add($"Operation {operation} with {index} took {time.TotalSeconds} for {amount}");
+            } else
+            {
+                ShowMessage("Enter number above zero");
+            }
+        }
+
+        private int GetValueFromTextField()
+        {
+            if (text != null && text.Length > 0)
+            {
+                if (int.TryParse(text, out int val))
+                {
+                    return val;
+                }
+                else
+                {
+                    ShowMessage("Enter correct number");
+                }
+            }
+            else
+            {
+                ShowMessage("Enter any number!");
+            }
+            return -1;
+        }
+        
+        private TimeSpan TimeMeasure<T>(Action<T> func, T args) 
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            func(args);
+            
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+        private void GenerateEntries(int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; ++i)
+            {
+                try
+                {
+                    string query = insertQeuries[random.Next(0, insertQeuries.Length - 1)];
+                    MakeQuery(query);
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage(ex.Message);
+                }
+            }
+        }
+
+        private void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void SelectButtonClicked(object sender, EventArgs e)
+        {
+            int amount = GetValueFromTextField();
+            var totalTime = TimeMeasure(selectMethod, amount);
+            
+            listBox1.Items.Add($"Operation {operation} with {index} took {totalTime.TotalSeconds} for {amount}");
+            ShowMessage($"Generation finished with {totalTime.TotalSeconds}");
+        }
+
+        private void selectMethod(int amount)
+        {
+           
+            operation = Operation.select;
+            TimeSpan totalTime = TimeSpan.FromMilliseconds(0);
+            Random random = new Random();
+
+            for (int i = 0; i < amount; ++i)
+            {
+                    MakeQuery($"select * from public.{Table.tableName} where {Table.firstColumn} = {random.Next(0, 40000)}");
+            }
+        }
+
+        private void GenerateInsertQueries(int amount)
+        {
+            Random random = new Random();
+            insertQeuries = new string[amount];
+            StringBuilder builder = new StringBuilder();
+            string[] result = new string[amount];
+            for (int i = 0; i < amount; ++i)
+            {
+                builder.Clear();
+                builder.Append(random.Next(1, 3));
+                builder.Append(",");
+                builder.Append($"'{sentences[random.Next(1, sentences.Keys.Count - 1)]}'");
+                string s = builder.ToString();
+
+                insertQeuries[i] = $"insert into {Table.tableName} ({Table.secondColumn}, {Table.thirdColumn}) values ({s});";
+            }
+        }
+
+        private void PopulateStringValues()
+        {
+            int wordsAmount = 100;
+            int sentenceAmount = 40;
+            var random = new Random();
+
+            string[] words = new string[wordsAmount];
+
+            while (wordsAmount > 0)
+            {
+                var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Clear();
+                for (int i = 0; i < random.Next(4, 8); i++)
+                {
+                    stringBuilder.Append(chars[random.Next(chars.Length)]);
+                }
+                words[wordsAmount - 1] = stringBuilder.ToString();
+                wordsAmount--;
+            }
+
+            for(int i = 0;i < sentenceAmount;++i)
+            {
+                sentences[i] = $"{words[random.Next(0, 99)]} {words[random.Next(0, 99)]} {words[random.Next(0, 99)]}";
+            }
+        }
+
+        private void DropIndex()
+        {
+            MakeQuery($"drop index if exists {Table.indexName}");
+        }
+
+        private void RadioButtonCheckedChanged(object sender, EventArgs e)
+        {
+            DropIndex();
+            if (radioButton2.Checked)
+            {
+                index = Index.btree;
+            } else if (radioButton3.Checked)
+            {
+                index = Index.hash;
+            } else if (radioButton4.Checked)
+            {
+                index = Index.gin;
+            } else
+            {
+                index = Index.noIndex;
+            }
+            CreateIndex(index);
+        }
+
+        private void SelectByConditionButtonClicked(object sender, EventArgs e)
+        {
+            operation = Operation.selectByCondition;
+            int amount = GetValueFromTextField();
+            var totalTime = TimeMeasure(conditionSelect, amount);
+           
+            listBox1.Items.Add($"Operation {operation} with {index} took {totalTime.TotalSeconds} for {amount}");
+            ShowMessage($"Generation finished with {totalTime.TotalSeconds}");
+        }
+
+        private void conditionSelect(int amount)
+        {
+            Random random = new Random();
+            for (int i = 0; i < amount; ++i)
+            {
+                MakeQuery($"select * from public.{Table.tableName} where {Table.firstColumn} < {random.Next(1, 100000)} AND {Table.firstColumn} > {random.Next(1, 50000)};");            
+            }
+        }
+
+        private void CreateIndex(Index index)
+        {
+
+            string column = Table.firstColumn;
+            string idx = "";
+            switch (index) 
+            {
+                case Index.btree:
+                    {
+                        idx = "btree";
+                        break;
+                    }
+                case Index.hash:
+                    {
+                        idx = "hash";
+                        break;
+                    }
+                case Index.gin:
+                    {
+                        idx = "gin";
+                        column = Table.thirdColumn;
+                        MakeQuery($"create index {Table.indexName} on public.{Table.tableName} using {idx}({column} gin_trgm_ops)");
+                        
+                        break;
+                    }
+            }
+
+            if (idx.Length > 0 && idx != "gin")
+                MakeQuery($"create index {Table.indexName} on public.{Table.tableName} using {idx}({column})");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int amount = GetValueFromTextField();
+            var totalTime = TimeMeasure(thirdColSelect, amount);
+            
+            listBox1.Items.Add($"Operation {operation} with {index} took {totalTime.TotalSeconds} for {amount}");
+            ShowMessage($"Generation finished with {totalTime.TotalSeconds}");
+        }
+
+        private void thirdColSelect(int amount)
+        {
+            TimeSpan totalTime = TimeSpan.FromMilliseconds(0.0);
+            Random random = new Random();
+            for (int i = 0; i < amount; ++i)
+            {
+                var q = $"select * from public.{Table.tableName} where {Table.thirdColumn} like '{sentences[random.Next(1, sentences.Count)]}';";
+                MakeQuery(q);
+            }
+        }
+    }
+
+    public static class Table
+    {
+        public const string
+            tableName = "test_table",
+            firstColumn = "idx2",
+            secondColumn = "second_field",
+            indexName = "index_name",
+            thirdColumn = "third_field";
+    }
+
+    public enum Index
+    {
+        noIndex,
+        hash, 
+        btree,
+        gin
+    }
+
+    public enum Operation
+    {
+        select, 
+        insert,
+        selectByCondition
+    }
+}
